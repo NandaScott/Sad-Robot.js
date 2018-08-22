3e9ad7fe22d1f05125a278bfe1d6f468cc54e92a
Nanda Scott <nanda.scott@passportinc.com>
2018-08-22 13:00:03 -0400

    Now added future support for modes, no modes implemented though

--- bot.js
+++ bot.js
@@ -18,56 +18,67 @@ client.on('message', (msg) => {
     let cardsFound = msg.content
         .match(/\[\[\b[\w\s\'\.|]+\b\]\]/g)
         .map((card) => {
-        return card.slice(2, card.indexOf(']]'));
-    });
+            let parts = card.match(/\w+/g);
+
+            return {
+                card: parts[0],
+                mode: parts[1]
+            };
+        });
+
+    console.log(cardsFound);
 
     for (let i = 0; i < cardsFound.length; i++) {
         setTimeout(() => {
 
             let startTimer = new Date().getTime();
 
-            axios.get(`${scryfallBaseUrl}/cards/named`, {
-                params: {
-                    fuzzy: cardsFound[i]
-                }
-            })
-            .then(function success(response) {
-                // Number of seconds it took to complete the get request
-                // displayed as a float.
-                let seconds = parseFloat(((new Date().getTime() - startTimer) / 1000) % 60);
-
-                let scryfall = response.data;
-
-                switch ("card_faces" in scryfall) {
-
-                    case true:
-                        for (let i = 0; i < scryfall.card_faces.length; i++) {
-                            let embed = new Discord.RichEmbed()
-                                .setImage(scryfall.card_faces[i].image_uris.normal)
-                                .setURL(scryfall.scryfall_uri)
-                                .setTitle(`**${scryfall.card_faces[i].name}**`)
-                                .setColor(0x1b6f9)
-                                .setFooter(`Fetch took: ${seconds} seconds.`);
-
-                            msg.channel.send({ embed })
+            switch (cardsFound[i].mode) {
+                default:
+                    axios.get(`${scryfallBaseUrl}/cards/named`, {
+                        params: {
+                            fuzzy: cardsFound[i].card
                         }
-                        break;
+                    })
+                    .then((response) => {
+                        // Number of seconds it took to complete the get request
+                        // displayed as a float.
+                        let seconds = parseFloat(((new Date().getTime() - startTimer) / 1000) % 60);
 
-                    case false:
-                        let embed = new Discord.RichEmbed()
-                            .setImage(scryfall.image_uris.normal)
-                            .setURL(scryfall.scryfall_uri)
-                            .setTitle(`**${scryfall.name}**`)
-                            .setColor(0x1b6f9)
-                            .setFooter(`Fetch took: ${seconds} seconds.`);
-        
-                        msg.channel.send({ embed });
-                        break;
-                }
-            })
-            .catch(function fail(error) {
-                msg.channel.send(error.response.data.details);
-            });
+                        let scryfall = response.data;
+
+                        switch ("card_faces" in scryfall) {
+
+                            case true:
+                                for (let i = 0; i < scryfall.card_faces.length; i++) {
+                                    let embed = new Discord.RichEmbed()
+                                        .setImage(scryfall.card_faces[i].image_uris.normal)
+                                        .setURL(scryfall.scryfall_uri)
+                                        .setTitle(`**${scryfall.card_faces[i].name}**`)
+                                        .setColor(0x1b6f9)
+                                        .setFooter(`Fetch took: ${seconds} seconds.`);
+
+                                    msg.channel.send({ embed })
+                                }
+                                break;
+
+                            case false:
+                                let embed = new Discord.RichEmbed()
+                                    .setImage(scryfall.image_uris.normal)
+                                    .setURL(scryfall.scryfall_uri)
+                                    .setTitle(`**${scryfall.name}**`)
+                                    .setColor(0x1b6f9)
+                                    .setFooter(`Fetch took: ${seconds} seconds.`);
+                
+                                msg.channel.send({ embed });
+                                break;
+                        }
+                    })
+                    .catch((error) => {
+                        msg.channel.send(error.response.data.details);
+                    });
+                    break;
+            }
         }, 100);
     }
 });
