714d8158ef0f3f15571e5d176434c7858a547fe7
Nanda Scott <nanda1123@gmail.com>
2018-08-21 23:13:07 -0400

    Added timing

--- bot.js
+++ bot.js
@@ -23,24 +23,30 @@ client.on('message', (msg) => {
 
     for (let i = 0; i < cardsFound.length; i++) {
         setTimeout(() => {
+
+            let startTimer = new Date().getTime();
+
             axios.get(`${scryfallBaseUrl}/cards/named`, {
                 params: {
                     fuzzy: cardsFound[i]
                 }
             })
             .then((response) => {
+                let elapsed = new Date().getTime() - startTimer;
+                let seconds = parseFloat((elapsed / 1000) % 60);
+
                 let scryfall = response.data;
 
                 let embed = new Discord.RichEmbed()
                     .setImage(scryfall.image_uris.normal)
                     .setURL(scryfall.scryfall_uri)
                     .setTitle(`**${scryfall.name}**`)
-                    .setColor(0x1b6f9);
+                    .setColor(0x1b6f9)
+                    .setFooter(`Fetch took: ${seconds} seconds.`);
 
                 msg.channel.send({embed});
             })
             .catch((error) => {
-                console.log(error.response.data);
                 msg.channel.send(error.response.data.details)
             });
         }, 100);
