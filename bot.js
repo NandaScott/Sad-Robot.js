f6ad830bfeb1917eb200543fcc607a2a805be096
Nanda Scott <nanda.scott@passportinc.com>
2018-08-22 12:33:32 -0400

    Bot now gets double faced cards correctly

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', (msg) => {
     }
 
     let cardsFound = msg.content
-        .match(/\[\[\b[\w\s\'\.]+\b\]\]/g)
+        .match(/\[\[\b[\w\s\'\.|]+\b\]\]/g)
         .map((card) => {
         return card.slice(2, card.indexOf(']]'));
     });
@@ -31,23 +31,42 @@ client.on('message', (msg) => {
                     fuzzy: cardsFound[i]
                 }
             })
-            .then((response) => {
-                let elapsed = new Date().getTime() - startTimer;
-                let seconds = parseFloat((elapsed / 1000) % 60);
+            .then(function success(response) {
+                // Number of seconds it took to complete the get request
+                // displayed as a float.
+                let seconds = parseFloat(((new Date().getTime() - startTimer) / 1000) % 60);
 
                 let scryfall = response.data;
 
-                let embed = new Discord.RichEmbed()
-                    .setImage(scryfall.image_uris.normal)
-                    .setURL(scryfall.scryfall_uri)
-                    .setTitle(`**${scryfall.name}**`)
-                    .setColor(0x1b6f9)
-                    .setFooter(`Fetch took: ${seconds} seconds.`);
+                switch ("card_faces" in scryfall) {
 
-                msg.channel.send({embed});
+                    case true:
+                        for (let i = 0; i < scryfall.card_faces.length; i++) {
+                            let embed = new Discord.RichEmbed()
+                                .setImage(scryfall.card_faces[i].image_uris.normal)
+                                .setURL(scryfall.scryfall_uri)
+                                .setTitle(`**${scryfall.card_faces[i].name}**`)
+                                .setColor(0x1b6f9)
+                                .setFooter(`Fetch took: ${seconds} seconds.`);
+
+                            msg.channel.send({ embed })
+                        }
+                        break;
+
+                    case false:
+                        let embed = new Discord.RichEmbed()
+                            .setImage(scryfall.image_uris.normal)
+                            .setURL(scryfall.scryfall_uri)
+                            .setTitle(`**${scryfall.name}**`)
+                            .setColor(0x1b6f9)
+                            .setFooter(`Fetch took: ${seconds} seconds.`);
+        
+                        msg.channel.send({ embed });
+                        break;
+                }
             })
-            .catch((error) => {
-                msg.channel.send(error.response.data.details)
+            .catch(function fail(error) {
+                msg.channel.send(error.response.data.details);
             });
         }, 100);
     }
