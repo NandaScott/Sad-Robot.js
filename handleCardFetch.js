12ce1d88882496f0caf7acbc6bc54296fc88cdbd
Nanda Scott <nanda1123@gmail.com>
2018-08-28 23:26:27 -0400

    Bugfixes

--- handleCardFetch.js
+++ handleCardFetch.js
@@ -4,19 +4,25 @@ const scryfallBaseUrl = 'https://api.scryfall.com';
 
 function handleCardFetch(msg) {
     let cardsFound = msg.content
-    .match(/\[\[\b[\w\s\'\.|]+\b\]\]/g)
+    .match(/\[\[\b[\w\s\'\.\,|]+\b\]\]/g)
     .map((card) => {
-        let parts = card.match(/[\w\s]+/g);
+        let parts = card.match(/[\w\s\,\'\.]+/g);
+
+        if (parts.length < 2) {
+            return {
+                card: parts[0].trim(),
+                mode: ''
+            }
+        }
 
         return {
-            card: parts[0],
-            mode: parts[1]
+            card: parts[0].trim(),
+            mode: parts[1].trim()
         };
     });
 
     for (let i = 0; i < cardsFound.length; i++) {
         setTimeout(() => {
-
             let startTimer = new Date().getTime();
 
             switch (cardsFound[i].mode) {
