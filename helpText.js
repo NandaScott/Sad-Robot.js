b9491bcb1acfa8da7ac38be32d6cb9d434498a8d
Nanda Scott <nanda1123@gmail.com>
2018-08-29 00:54:16 -0400

    Updated help text to reflect regex rework

--- helpText.js
+++ helpText.js
@@ -9,11 +9,6 @@ Here is a list of patterns I look for:
 [[cardname]] ............ Fetch a card as an image
 [[cardname | oracle]] ... Fetch a card's oracle text
 [[cardname | price]] .... Fetch a card's price
-
-Keep in mind that there cannot be spaces in these spots
-[[ cardname | price ]]
-  ^                ^
-Otherwise I won't catch the card you're looking for.
 \`\`\`
 `;
 
