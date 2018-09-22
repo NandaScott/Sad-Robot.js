1ee3a08a9adf876904bff1ad138f2b83f136296f
Nanda Scott <nanda1123@gmail.com>
2018-09-22 16:44:18 -0400

    Fixed an error that could cause the bot to crash

--- handleCardFetch.js
+++ handleCardFetch.js
@@ -1,55 +1,57 @@
 const cardModes = require('./cardModes');
 
 function handleCardFetch(msg) {
-    let cardsFound = msg.content
-    .match(/(\[\[[\w\s\'\.\,|:]+\]\])+/g)
-    .map((card) => {
-        let parts = card.match(/[\w\s\,\'\.:]+/g);
+    let cardsFound = msg.content.match(/(\[\[[\w\s\'\.\,|:]+\]\])+/g);
 
-        if (parts.length < 2) {
+    if (cardsFound) {
+        cardsFound.map((card) => {
+            let parts = card.match(/[\w\s\,\'\.:]+/g);
+
+            if (parts.length < 2) {
+                return {
+                    card: parts[0].trim(),
+                    mode: '',
+                    setCode: ''
+                }
+            }
+
+            if (parts[1].includes('set:')) {
+                return {
+                    card: parts[0].trim(),
+                    mode: 'set',
+                    setCode: parts[1].split(':').pop().trim()
+                }
+            }
+
             return {
                 card: parts[0].trim(),
-                mode: '',
+                mode: parts[1].trim(),
                 setCode: ''
-            }
+            };
+        });
+
+        for (let i = 0; i < cardsFound.length; i++) {
+            setTimeout(() => {
+
+                switch (cardsFound[i].mode) {
+                    default:
+                        cardModes.checkCache(msg, cardsFound[i], 'image');
+                        break;
+                    case 'oracle':
+                        cardModes.checkCache(msg, cardsFound[i], 'oracle');
+                        break;
+                    case 'price':
+                        cardModes.checkCache(msg, scryfallBaseUrl, cardsFound[i], 'price');
+                        break;
+                    case 'set':
+                        cardModes.checkCache(msg, scryfallBaseUrl, cardsFound[i], 'set');
+                        break;
+                    case 'legal':
+                        cardModes.checkCache(msg, scryfallBaseUrl, cardsFound[i], 'legal');
+                        break;
+                }
+            }, 100);
         }
-
-        if (parts[1].includes('set:')) {
-            return {
-                card: parts[0].trim(),
-                mode: 'set',
-                setCode: parts[1].split(':').pop().trim()
-            }
-        }
-
-        return {
-            card: parts[0].trim(),
-            mode: parts[1].trim(),
-            setCode: ''
-        };
-    });
-
-    for (let i = 0; i < cardsFound.length; i++) {
-        setTimeout(() => {
-
-            switch (cardsFound[i].mode) {
-                default:
-                    cardModes.checkCache(msg, cardsFound[i], 'image');
-                    break;
-                case 'oracle':
-                    cardModes.checkCache(msg, cardsFound[i], 'oracle');
-                    break;
-                case 'price':
-                    cardModes.checkCache(msg, scryfallBaseUrl, cardsFound[i], 'price');
-                    break;
-                case 'set':
-                    cardModes.checkCache(msg, scryfallBaseUrl, cardsFound[i], 'set');
-                    break;
-                case 'legal':
-                    cardModes.checkCache(msg, scryfallBaseUrl, cardsFound[i], 'legal');
-                    break;
-            }
-        }, 100);
     }
 }
 
