12e9fb047b9a4843a4e68ea4352c66ea554b8729
Nanda Scott <nanda1123@gmail.com>
2018-09-25 21:43:57 -0400

    Made this file much less stupid

--- cardLayoutHandlers.js
+++ cardLayoutHandlers.js
@@ -1,3 +1,29 @@
+
+// Many of the card faces all have similar places for each attribute
+// however we need to rewrite the `base` object in memory because
+// otherwise we'll eventually have a very messy object in memory
+// so we call this to generate it, then modify values as needed, if any
+function generateBase(scryfallObject) {
+    let base = {
+        name: scryfallObject.name,
+        image: scryfallObject.image_uris.border_crop,
+        thumbnail: scryfallObject.image_uris.small,
+        url: scryfallObject.scryfall_uri,
+        power: ('power' in scryfallObject) ? scryfallObject.power : '',
+        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
+        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
+        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
+        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
+        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
+        legalities: scryfallObject.legalities,
+        typeLine: scryfallObject.type_line,
+        oracleText: scryfallObject.oracle_text,
+        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
+    }
+
+    return base;
+}
+
 function handleTransform(scryfallObject, returnArray=false) {
     if (returnArray) {
         let array = [];
@@ -145,234 +171,47 @@ function handleSplit(scryfallObject, returnArray=false) {
 }
 
 function handleNormal(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handlePlanar(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleScheme(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleEmblem(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleLeveler(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleSaga(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleVanguard(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleMeld(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleToken(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleHost(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleAugment(scryfallObject, returnArray=false) {
-    let base = {
-        name: scryfallObject.name,
-        image: scryfallObject.image_uris.border_crop,
-        thumbnail: scryfallObject.image_uris.small,
-        url: scryfallObject.scryfall_uri,
-        power: ('power' in scryfallObject) ? scryfallObject.power : '',
-        toughness: ('toughness' in scryfallObject) ? scryfallObject.toughness : '',
-        cost: ('mana_cost' in scryfallObject) ? scryfallObject.mana_cost : '',
-        usd: ('usd' in scryfallObject) ? scryfallObject.usd : 'N/A',
-        eur: ('eur' in scryfallObject) ? scryfallObject.eur : 'N/A',
-        tix: ('tix' in scryfallObject) ? scryfallObject.tix : 'N/A',
-        legalities: scryfallObject.legalities,
-        typeLine: scryfallObject.type_line,
-        oracleText: scryfallObject.oracle_text,
-        flavorText: ('flavor_text' in scryfallObject) ? scryfallObject.flavor_text : 'N/A'
-    }
-
-    return base;
+    return generateBase(scryfallObject);
 }
 
 function handleDoubleFacedToken(scryfallObject, returnArray=false) {
