b9e7ea041034ea25f68f8be6e9deefdf86b52566
Nanda Scott <nanda1123@gmail.com>
2018-11-16 14:44:00 -0500

    Updated help text

--- helpText.js
+++ helpText.js
@@ -1,33 +1,39 @@
 const helpText = (prefix) => `
 \`\`\`
 
-To ping a card, use the pattern [[cardname]].
+[[cardname]]
+    Fetch a card. Defaults to the image.
+[[cardname | oracle]]
+    Fetch a card's oracle text.
+[[cardname | price]]
+    Fetch a card's price.
+[[cardname | legal]]
+    Fetch a card's legality.
+[[cardname | rules]]
+    Fetch a card's rulings.
+[[cardname | flavor]]
+    Fetch a card's flavor text.
+[[cardname | unique]]
+    Fetch a linked list of all printings for a card.
+[[🎲]]
+    Fetches a random card.
 
-Here is a list of patterns I look for:
-
-[[cardname]] ............ Fetch a card as an image
-[[cardname | oracle]] ... Fetch a card's oracle text
-[[cardname | price]] .... Fetch a card's price
-[[cardname | legal]] .... Fetch a card's legality
-[[cardname | rules]] .... Fetch a card's rulings
-[[cardname | flavor]] ... Fetch a card's flavor text
-[[cardname | unique]] ... Fetch a linked list of all printings for a card
-[[🎲]] .................. Fetch a random card
-
-To get a card from a specific set, you can also use
-the following syntax with any other mode
+You may fetch a specific set of a card with the following pattern.
+You may use any other mode with this.
 [[cardname | set:KTK]]
 [[cardname | set:KTK | oracle]]
-[[cardname | set:KTK | price]]
 
 Other commands:
-${prefix}help .................... Shows this help text
-${prefix}ping .................... Tests the bot's connection to discord
-${prefix}prefix .................. Used for setting this server's command prefix. Admins only.
-resetprefix ...................... Resets this server's command prefix to default. Admins only.
-${prefix}feedback <msg> .......... Sends the maintainer of Sad Robot a DM with your issue.
-                          Note that abuse/spam of this command will lead to an
-                          account ban for this bot.
+${prefix}help
+    Shows this help text.
+${prefix}ping
+    Tests the bot's connection to Discord.
+${prefix}prefix
+    Sets the server's command prefix. Admins only.
+    Admins may reset the prefix with the message 'resetprefix'.
+${prefix}feedback
+    Sends feedback to the maintainer of Sad Robot.
+    Abuse of this will result in a ban.
 \`\`\`
 `;
 
